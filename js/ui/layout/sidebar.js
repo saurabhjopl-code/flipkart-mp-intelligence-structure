c3082a6a52ad6b24e6773a9d120342bdac6373c8
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-18 12:47:25 +0530

    Update sidebar.js

--- js/ui/layout/sidebar.js
+++ js/ui/layout/sidebar.js
@@ -1,3 +1,10 @@
+document.querySelectorAll(".expandable").forEach(menu => {
+  menu.addEventListener("click", function () {
+    const subMenu = this.querySelector(".sub-menu");
+    subMenu.style.display = subMenu.style.display === "block" ? "none" : "block";
+  });
+});
+
 document.querySelectorAll(".nav-item").forEach(item => {
   item.addEventListener("click", function () {
     document.querySelectorAll(".nav-item").forEach(i => i.classList.remove("active"));
