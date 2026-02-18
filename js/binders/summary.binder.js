c154912511283c8a2683abbdf495d9b8a8eef57b
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-18 14:40:13 +0530

    Create summary.binder.js

---
+++ js/binders/summary.binder.js
@@ -0,0 +1,51 @@
+import { getRevenueSummary } from "../summaries/revenue.summary.js";
+import { getReturnSummary } from "../summaries/return.summary.js";
+import { getSpendSummary } from "../summaries/spend.summary.js";
+import { getRoiSummary } from "../summaries/roas.summary.js";
+import { getSpendControlSummary } from "../summaries/spendControl.summary.js";
+
+function formatCurrency(value) {
+  return "₹" + value.toLocaleString("en-IN", { maximumFractionDigits: 0 });
+}
+
+function formatPercent(value) {
+  return value.toFixed(2) + "%";
+}
+
+function waitForData() {
+  const interval = setInterval(() => {
+    if (window.dataStore && window.dataStore.isLoaded) {
+      clearInterval(interval);
+      renderSummaries();
+    }
+  }, 200);
+}
+
+function renderSummaries() {
+
+  const revenue = getRevenueSummary();
+  const returnData = getReturnSummary();
+  const spend = getSpendSummary();
+  const roi = getRoiSummary();
+  const spendControl = getSpendControlSummary();
+
+  document.getElementById("totalRevenue").innerText =
+    formatCurrency(revenue.totalRevenue);
+
+  document.getElementById("returnPercent").innerText =
+    formatPercent(returnData.returnPercent);
+
+  document.getElementById("netSales").innerText =
+    formatCurrency(revenue.netSales);
+
+  document.getElementById("adSpend").innerText =
+    formatCurrency(spend.totalSpend);
+
+  document.getElementById("roiValue").innerText =
+    roi.roi.toFixed(2);
+
+  document.getElementById("spendPercent").innerText =
+    formatPercent(spendControl.spendPercent);
+}
+
+waitForData();
