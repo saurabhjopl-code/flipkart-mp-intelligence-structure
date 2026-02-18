b40d16a9e1f864ab873770a3500a190122dfa0d8
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-18 14:49:40 +0530

    Update spendControl.summary.js

--- js/summaries/spendControl.summary.js
+++ js/summaries/spendControl.summary.js
@@ -1,22 +1,12 @@
+import { aggregateGMV } from "../aggregations/gmv.aggregation.js";
+import { aggregateSpend } from "../aggregations/spend.aggregation.js";
+import { calculateSpendPercent } from "../calculations/spendPercent.calculation.js";
+
 export function getSpendControlSummary() {
+  const { netSales } = aggregateGMV();
+  const { totalSpend } = aggregateSpend();
 
-  const GMV = window.dataStore.GMV || [];
-  const CDR = window.dataStore.CDR || [];
-
-  let netSales = 0;
-  let totalSpend = 0;
-
-  GMV.forEach(row => {
-    netSales += Number(row["Final Sale Amount"] || 0);
-  });
-
-  CDR.forEach(row => {
-    totalSpend += Number(row["Ad Spend"] || 0);
-  });
-
-  const spendPercent = netSales > 0
-    ? (totalSpend / netSales) * 100
-    : 0;
+  const spendPercent = calculateSpendPercent(totalSpend, netSales);
 
   return {
     spendPercent
