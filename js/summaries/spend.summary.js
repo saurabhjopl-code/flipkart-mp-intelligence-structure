a2adcad065760ed6b343522bb2cefcac501486a0
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-18 14:40:54 +0530

    Update spend.summary.js

---
+++ js/summaries/spend.summary.js
@@ -0,0 +1,14 @@
+export function getSpendSummary() {
+
+  const CDR = window.dataStore.CDR || [];
+
+  let totalSpend = 0;
+
+  CDR.forEach(row => {
+    totalSpend += Number(row["Ad Spend"] || 0);
+  });
+
+  return {
+    totalSpend
+  };
+}
